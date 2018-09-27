8ad09af43bbd83469b237a11ac8a4674022c4046
León Darío Peña L <leonplondon@gmail.com>
2018-09-26 23:00:21 -0500

    Fixed problem with email service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -4,7 +4,7 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 
 import { UserInterface } from '../models/user.interface';
-import { EmailService } from 'email/email.service';
+import { EmailService } from '../email/email.service';
 
 @Injectable()
 export class UsersService {
